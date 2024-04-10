070540d7e96160285b85f5d744942256317eb719
ClaudioPerrone <cla-agadir97@hotmail.it>
2024-04-10 16:47:11 +0200

    Feat: created a function with a for-cicle for request 10 email

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
+/* 
+Esercizio di oggi: Vue Email List
+
+- Attraverso l'apposita API di Boolean "https://flynn.boolean.careers/exercises/api/random/mail"
+- generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+*/
+
+
 const { createApp } = Vue;
 
-createApp(
+createApp({
     data() {
-        
         return {
+            //INPUT DATI
+            //Array vuoto da riempire con le email
+            emails:[]
+    };
+    },
+    methods: {
+        //ELABORAZIONE DATI
+        //Creo funzione con un ciclo for per chiedere 10 email e inserirli nell'array
+        getEmailFromAPI(){
+            for (let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.emails.push(response.data.response);
+                });
+            }
+        }
+    },
+    //OUTPUT DATI
+    //Funzione evocata
+    mounted() {
+        this.getEmailFromAPI();
     }
 
 }).mount('#app')
